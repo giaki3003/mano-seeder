54e025520b828dc95446a6468532b070592ac102
Giacomo Milligan <giaki3003@gmail.com>
2019-09-05 14:54:43 +0200

    [maintenance] Keep proto versions on par with current master

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70001
+#define REQUIRE_VERSION 70007
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 1 : 1;
+    return testnet ? 1 : 290000;
 }
 
 std::string static inline ToString(const CService &ip) {
